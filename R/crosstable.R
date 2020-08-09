682b6b24bbe785d3cb804658770e7328cac9fb1c
Lemon <54917401+lemonlinn@users.noreply.github.com>
2020-08-09 00:28:12 -0400

    setup and adding first functions

---
+++ R/crosstable.R
@@ -0,0 +1,48 @@
+#' Generate a table for cross tabulations
+#'
+#' This function generates a flextable for a cross tabulation
+#' between two categorical (nominal or ordinal), numeric variables within a dataframe.
+#' There is an option to make the column names of the table reflect response options
+#' instead of the default, where the column names are the actual numeric values.
+#'
+#' @param data Object holding the dataframe
+#' @param x String of the first variable name
+#' @param y String of the second variable name
+#' @param x_names Vector of response options. Default value is FALSE.
+#' @param y_names Vector of response options. Default value is FALSE.
+#' @return A flextable object
+#' @export
+crosstable <- function(data, x, y, x_names = FALSE, y_names = FALSE){
+  tab = xtabs(formula = ~unlist(data[y])+unlist(data[x]))
+  tabDF = as.data.frame.matrix(tab)
+
+  if (!isFALSE(y_names)){
+    rownames(tabDF) <- c(y_names)
+  }
+
+  tabDF <- cbind(x = rownames(tabDF), tabDF)
+
+  if (!isFALSE(x_names)){
+    colnames(tabDF) <- c(y, x_names)
+  }
+
+  chitest = summary(tab)
+  title = paste0("Cross Tabulation of ", x, " and ", y)
+  foot = paste0("(n=", toString(nrow(na.omit(data[c(x,y)]))), ")")
+  chi = paste0("Chi-Square=", round(chitest$statistic,2), " (p-val=", round(chitest$p.value,4), ")")
+  maxval = ncol(tabDF)
+
+  ft <- flextable::flextable(tabDF)
+
+  ft <- flextable::add_header_row(ft, top = TRUE, values = c(NA, x), colwidths = c(1, maxval-1))
+  ft <- flextable::add_header_row(ft, top = TRUE, values = title, colwidths = maxval)
+  ft <- flextable::align(ft, i = c(1:2), align = "center", part = "header")
+  ft <- flextable::add_footer_row(ft, top = FALSE, values = c(chi, foot), colwidths = c(maxval-1, 1))
+  ft <- flextable::align(ft, i = 1, j = maxval, align = "right", part = "footer")
+
+  #ft <- border_remove(ft)
+  ft <- bold(ft, part = "header", i = 1)
+  #ft <- hline_bottom(ft, border = fp_border())
+  #ft <- hline(ft, i = 1, border = fp_border(), part = "header")
+  return(ft)
+}
---
+++ R/unitable.R
@@ -0,0 +1,42 @@
+#' Generate a table for a single variable
+#'
+#' This function generates a flextable for the distribution of responses to a
+#' single categorical (nominal or ordinal) numeric variable of a dataframe.
+#'
+#' @param data Object holding the dataframe
+#' @param colval Call to the variable in the dataframe formatted as df$colname
+#' @param minval Integer representing the smallest possible numeric value in the variable
+#' @param maxval Integer representing the largest possible numeric value in the variable
+#' @param namecol Vector containing string of response options. Must be the same length as maxval-minval.
+#' @param text String of the question wording in the survey
+#' @param tblnum Integer representing the table number in the series
+#' @param title String of the table title, usually the variable name in the dataframe
+#' @return A flextable object
+#' @export
+unitable <- function(data, colval, minval, maxval, namecol, text, tblnum, title) {
+  myvec <- vector("character", 1)
+  myprop <- vector("character", 1)
+  for (val1 in minval:maxval){
+    myvec[val1] <- paste("(n=",toString(nrow(data[(colval == val1), ])),")", sep = "")
+    myprop[val1] <- sprintf("%0.1f%%",(nrow(data[(colval == val1), ])/length(colval))*100)
+  }
+  myprop <- append(myprop, text, after = 0)
+  myvec <- append(myvec, paste("(n=", toString(length(colval)), ")", sep = ""), after = 0)
+  mydf <- data.frame(t(data.frame(myprop, myvec)))
+  names(mydf) <- namecol
+
+  ft <- flextable(mydf)
+  ft <- add_header_row(ft, top = TRUE, values = title, colwidths = c(maxval+1))
+  ft <- add_header_row(ft, top = TRUE, values = paste("Table #", toString(tblnum), sep = ""), colwidths = c(maxval+1))
+  ft <- align(ft, i = c(1:2), align = "center", part = "header")
+  #ft <- width(ft, j = c(1), width = 2.5)
+  #ft <- height(ft, i = c(1), height = 2, part = "body")
+  ft <- autofit(ft)
+  ft <- border_remove(ft)
+  ft <- border_inner_v(ft, border = fp_border(), part = "body")
+  ft <- border(ft, i = 3, j = c(1:maxval), border.right = fp_border(), part = "header")
+  ft <- hline_bottom(ft, border = fp_border())
+  ft <- hline(ft, i = 3, j = c(1:maxval+1), border = fp_border(), part = "header")
+  ft <- bold(ft, i = c(1:3), part = 'header')
+  return(ft)
+}
